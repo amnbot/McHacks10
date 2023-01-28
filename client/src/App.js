70c1f56d7624fa71e5f358bfd645e5b2bc4718fd
AMNbot <aymenouali01@gmail.com>
2023-01-28 15:43:08 -0500

    Sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/Counter";
+import TemporaryDrawer from "./UI/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <header className="App-header">
         <button className="bg-slate-600 px-4 py-2 rounded-[15px]" onClick={() => setCount(count + 1)}>ADD</button>
         <Counter count={count} />
+        <TemporaryDrawer />
       </header>
     </div>
   );
